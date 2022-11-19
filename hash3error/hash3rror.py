b7a71914b098265f9b94985a408d2f409f642ad9
Dion Roloff <113385591+dionroloff@users.noreply.github.com>
2022-11-18 22:09:50 -0800

    some changes

--- hash3error/hash3rror.py
+++ hash3error/hash3rror.py
@@ -1,10 +1,6 @@
-'''
-CTF Description:
-
-"we got this corrupted hash password from a Pcap file with a note (password = sha-1(hash-result)).
-
-HASH:77be5d24ed2e3e590045e1d6o7e84i50d2799c19f48ede46804a8734e287df120f"
-'''
+# CTF Description:
+# "we got this corrupted hash password from a Pcap file with a note (password = sha-1(hash-result)).
+# HASH:77be5d24ed2e3e590045e1d6o7e84i50d2799c19f48ede46804a8734e287df120f"
 
 # I observe that this hash looks like hex format, but it has two characters 'i' and 'o' which are not found in hexadecimal.
 # Removing these two characters from the string, I confirm that this is a 64-character long string, the length of a Sha256 hash.
